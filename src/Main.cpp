96ce1f13edd0b7293ae4d1468c81f7d14a4424af
miker83z <mikerz83@gmail.com>
2017-12-13 11:22:17 +0100

    update5

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,3 @@
-#include "form.h"
-#include "Agent.h"
-<<<<<<< HEAD
-#include "Matrix.h"
-=======
 #include <FL/Fl.H>
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Box.H>
@@ -10,9 +5,10 @@
 #include <math.h>
 #include <stdio.h>
 #include <time.h>
-#include <DrawAgents.h>
-
->>>>>>> 4e82b116715f55f7b4b7ac4a4572c713a051a00f
+#include "form.h"
+#include "Agent.h"
+#include "Matrix.h"
+#include "DrawAgents.h"
 
 const int N = 100;
 const int canti = 2;
@@ -33,11 +29,11 @@ int main(int argc, char **argv) {
 	//CreateMyWindow();
 	//Fl::add_idle(idle_cb, 0);
 	//Fl::run();
+	//Fl_Double_Window win(400, 400);
+	//MyTimer tim(10, 10, win.w() - 20, win.h() - 20);
+	//win.show();
+	return(Fl::run());
 	system("pause");
 	return 0;
-	Fl_Double_Window win(400, 400);
-	MyTimer tim(10, 10, win.w() - 20, win.h() - 20);
-	win.show();
-	return(Fl::run());
 }
 //-------------------------------------------------------------------------------------------------
