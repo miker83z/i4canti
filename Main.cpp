89f0997316665d2191b11e06326689c3fdcaa383
flamel13 <thesorcerer13@gmail.com>
2017-12-30 20:08:20 +0100

    Updated graphic part

--- Main.cpp
+++ Main.cpp
@@ -10,6 +10,7 @@
 #include "DrawAgents.h"
 #include "Agent.h"
 #include "Environment.h"
+#include "GUI.h"
 
 using namespace std;
 
@@ -20,8 +21,16 @@ const int NA = 50;		//Number of Agents
 const int STEP = 10;
 const int END_TIME = 200;
 
-//static unsigned i = 0;
-vector<DrawAgents*> allAgents;
+Windows window(Point(w, 0), w, h, "Part5");
+Line line1(Point(0, h / 4), Point(w, h / 4), 3, FL_WHITE);
+Line line2(Point(w / 2, 0), Point(w / 2, h / 4), 3, FL_WHITE);
+Text text1(Point(50, 50), FL_HELVETICA, 25, FL_RED, "Parametri");
+
+Slider slider1(Point(50, 80), 100, 30, "Velocità agenti");
+In_box inbox1(Point(50, 120), 100, 30, "Numero di agenti");
+
+#include "callback1.h"
+Button button1(Point(w / 2 + 200, 50), 100, 30, "Avvia", callback1);
 
 int main(int argc, char **argv) {
 	Environment* env = new Environment(N, NA, NC);
@@ -47,19 +56,18 @@ int main(int argc, char **argv) {
 	system("PAUSE");
 	return 0;
 	
-	/*Fl_Double_Window *window = new Fl_Double_Window(400, 400, "FLTK Test");
+	srand(time(NULL));
+	window.attach(line1);
+	window.attach(line2);
+	window.attach(text1);
 
-	for (i = 0; i < 10; i++) {
+	window.attach(slider1);
+	window.attach(inbox1);
+	window.attach(button1);
 
-		allAgents.push_back(new DrawAgents(10*i, 10*i, 50, 50, 0));
-
-	}
-
-	window->end();
-	Fl::visual(FL_DOUBLE|FL_INDEX);
-	window->show();
-
-	return(Fl::run());
-	*/
+	slider1.set_value(500);
+	slider1.set_bounds(100, 1000);
+	inbox1.set_value("10");
+	return Fl::run();
 }
 //-------------------------------------------------------------------------------------------------
